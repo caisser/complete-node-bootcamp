b7e157fd0060583363dbb738eea966496074404f
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-04-26 15:16:22 -0500

    backup

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+).replace('<USERNAME>', process.env.DATABASE_USERNAME);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Connectec to mongo successfully!'));
 
 // Start Server
 const port = process.env.PORT || 3000;
