1fa5e82fd8ccffd19ba57a9e861473a8ec22320d
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-05-16 22:30:58 -0500

    Aggregation pipeline: matching and grouping

--- 4-natours/starter/controllers/tours.controller.js
+++ 4-natours/starter/controllers/tours.controller.js
@@ -135,6 +135,46 @@ exports.deleteTour = async (req, res) => {
   }
 };
 
+// Aggregations pipelines
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: {
+          ratingsAverage: { $gte: 4.5 },
+        },
+      },
+      {
+        $group: {
+          //_id: null,
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        // sort by avgPrice asc
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Stats retrieved',
+      data: { stats },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
 // exports.updateTour = (req, res) => {
 //   const { id } = req.params;
 //   const { body } = req;
