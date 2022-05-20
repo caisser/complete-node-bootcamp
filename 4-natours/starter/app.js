c784f5743ab866d0d7d48facb0dd3932b2b25141
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-05-20 16:45:02 -0500

    Added error class and error controller for initial error handling

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError');
+const errorHandler = require('./controllers/error.controller');
 const toursRouter = require('./routes/tours.router');
 const usersRouter = require('./routes/users.router');
 
@@ -11,6 +12,7 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -18,5 +20,12 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/tours', toursRouter);
 app.use('/api/users', usersRouter);
+
+app.all('*', (req, res, next) => {
+  // Error custom class
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
 
 module.exports = app;
