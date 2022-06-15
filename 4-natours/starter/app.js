fbace44b74f7edf4150e126569328ea9d878a14c
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-06-14 22:20:07 -0500

    Added packages for data sanitization, security headers, xss and parameter pollution

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const errorHandler = require('./controllers/error.controller');
@@ -10,20 +14,46 @@ const usersRouter = require('./routes/users.router');
 const app = express();
 
 // Global Middlewares
+// Security HTTP headers
+app.use(helmet());
 
+// Logs for Development
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit request from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 3600000,
   message: 'Too many request from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS (Cross site scripting)
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Routes
